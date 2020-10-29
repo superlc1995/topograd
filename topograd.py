5f3a4039c1c3e9271b3c0ef5176c9fc02fd14474
superlc1995 <46761885+superlc1995@users.noreply.github.com>
2020-10-28 20:13:14 -0500

    Add files via upload

--- topograd.py
+++ topograd.py
@@ -4,6 +4,7 @@ from numpy import array
 from numba import cuda
 import numpy as np
 from numpy import argsort
+import torch
 import faiss
 
 # pc is the dataset, which should be a point cloud
@@ -228,3 +229,128 @@ def rips_graph(point_cloud, k):
     cpuindex.add(point_cloud)
 
     return cpuindex.search(point_cloud, k)
+
+class topoclustergrad(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, pc, k1,k2,tau1, destnum):
+        
+        pc = pc.detach().numpy()
+#         print(pc)
+        f,I1 = approximate(pc, k2,tau1)
+#         print('this is f: ')
+#         print(f)
+#         print('this is I1')
+#         print(I1)
+        f = f.astype(float)
+        pc = pc.astype(float)
+        sorted_idxs1 = np.argsort(f)
+#         print('this is I1')
+#         print(I1)
+#         newi1 = newI1(I1, sorted_idxs1 )
+#         print('this is newi1')
+#         print(newi1)
+        I1 = newI1(I1, sorted_idxs1 )
+        f = f[sorted_idxs1]
+        pc = pc[sorted_idxs1]
+#         print('this is pc: ')
+#         print(pc)
+#         print('this is I1')
+#         print(I1)
+#         print(pc[recover(sorted_idxs1)])
+        lims, I = rips_graph(pc, k1)
+#             print(lims)
+        ddd,ddqq = Clustering(f,I, lims, 1)
+        see = []
+        for i in ddqq:
+            if (i == np.array([-1,-1])).all():
+                pass
+            else:
+                see.append(i)
+        see = np.array(see)
+        result = []
+        for i in np.unique(see[:,0]):
+            result.append([see[np.where(see[:,0] == i)[0]][0,0], max(see[np.where(see[:,0] == i)[0]][:,1]) ])
+        result = np.array(result)
+        pdpairs = result
+        oripd = f[result]
+        sorted_idxs = np.argsort(oripd[:,0] - oripd[:,1])
+        changing = sorted_idxs[:-destnum]
+        nochanging = sorted_idxs[-destnum:-1]
+        biggest = oripd[sorted_idxs[-1]]
+        dest = np.array([biggest[0], biggest[1]])
+        changepairs = pdpairs[changing]
+#         print('this is chag')
+#         print(changepairs)
+        nochangepairs = pdpairs[nochanging]
+#         print(sorted_idxs1)
+#         print(nochangepairs)
+        pd11 = f[changepairs]
+        weakdist = np.sum(pd11[:,0] - pd11[:,1])/np.sqrt(2)
+        strongdist = np.sum(np.linalg.norm(f[nochangepairs] - dest,axis = 1)) 
+#         print(strongdist)
+        ctx.save_for_backward(torch.tensor(pc),torch.tensor(tau1),torch.tensor(changepairs),torch.tensor(nochangepairs),torch.tensor(dest),torch.tensor(I1),torch.tensor(f),torch.tensor(sorted_idxs1))
+        return torch.tensor(weakdist + strongdist)
+    @staticmethod
+    def backward(ctx, grad_output):
+#         print(grad_output)
+        pc,tau1,changepairs,nochangepairs,dest,I1,f,sorted_idxs1 = ctx.saved_tensors
+        pc = pc.numpy()
+#         print(pc)
+        tau1 = float(tau1)
+        changepairs = changepairs.numpy()
+        nochangepairs = nochangepairs.numpy()
+        dest = dest.numpy()
+        I1 = I1.numpy()
+        sorted_idxs1 = sorted_idxs1.numpy()
+#         print(I1)
+        f = f.numpy()
+        grad_input = np.zeros(pc.shape)
+#         print(pc)
+        for i in changepairs:
+#             print(i)
+#             print(I1[i[0]])
+            coeff = np.sqrt(2)/len(changepairs) * np.exp( -np.linalg.norm(pc[i[0]] - pc[I1[i[0]]],axis = 1) ** 2 / tau1)
+            direction = pc[i[0]] - pc[I1[i[0]]]     
+#             print(direction)
+            grad_input[I1[i[0]]] += multp(direction, coeff)
+            coeff1 = -np.sqrt(2)/len(changepairs) * np.exp( -np.linalg.norm(pc[i[1]] - pc[I1[i[1]]],axis = 1) ** 2 / tau1)
+            direction1 = pc[i[1]] - pc[I1[i[1]]]
+            grad_input[I1[i[1]]] += multp(direction1, coeff1)
+#         print(nochangepairs)
+#         print('this is pc')
+#         print(pc)     
+        for i in nochangepairs:
+            dist = np.linalg.norm(f[i] - dest) 
+#             print(dist)
+            if dist == 0:
+                pass
+            else:
+                coeff = 1/dist * (f[i[0]] - dest[0]) /tau1/len(nochangepairs) * np.exp( -np.linalg.norm(pc[i[0]] - pc[I1[i[0]]],axis = 1) ** 2 / tau1)
+                direction = pc[i[0]] - pc[I1[i[0]]]
+                grad_input[I1[i[0]]] += multp(direction, coeff)
+                coeff1 = 1/dist * (f[i[1]] - dest[1]) /tau1 /len(nochangepairs)* np.exp( -np.linalg.norm(pc[i[1]] - pc[I1[i[1]]],axis = 1) ** 2 / tau1)
+                direction1 = pc[i[1]] - pc[I1[i[1]]]
+                grad_input[I1[i[1]]] += multp(direction1, coeff1)
+#         print(grad_input)
+        grad_input = grad_input[recover(sorted_idxs1)]
+#         print(grad_input)
+#         print(pc[recover(sorted_idxs1)])
+#         print(pc)
+#         print(sorted_idxs1)
+#         print(torch.tensor(grad_input,dtype = torch.float64))
+        return torch.tensor(grad_input,dtype = torch.float64),None,None,None,None
+
+def newI1(I1, sortrule):
+    newI1 = I1[sortrule]
+    for i in range(newI1.shape[0]):
+        for j in range(newI1.shape[1]):
+            newI1[i,j]= np.where(sortrule == newI1[i,j])[0][0]
+    return newI1
+
+def recover(xxx):
+    ffff = []
+    for i in range(len(xxx)):
+        ffff.append(np.where(xxx == i)[0][0])
+
+    ffff = np.array(ffff)  
+    return ffff
